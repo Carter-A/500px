245b2dfafd483abe7dbafd289f39f7b19d822896
Carter-A <carter.s.andrew@gmail.com>
2016-02-03 14:42:17 -0500

    Grid display of photos. Navbar. Styling to match 500px.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def index
     base_url = 'https://api.500px.com'
-    popular_url = '/v1/photos?feature=popular&sort=created_at&rpp=100&image_size=3&include_store=store_download&include_states=voted'
+    popular_url = '/v1/photos?feature=popular&sort=created_at&rpp=100&image_size=4&include_store=store_download&include_states=voted'
     consumer_key = configatron.consumer_key
 
     response = HTTParty.get("#{base_url}#{popular_url}#{consumer_key}")
