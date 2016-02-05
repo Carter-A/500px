ae5813a2653998d99f18389b705b1fdde75591e0
Carter-A <carter.s.andrew@gmail.com>
2016-02-05 15:36:51 -0500

    first basic index test

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -4,4 +4,10 @@ class PhotosControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:photos)
+  end
+
 end
